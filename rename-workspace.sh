4eb65d7dea27b9ea9035d4c08f9b650347db3080
Hugo <hugo.hornquist@gmail.com>
2016-02-16 19:04:27 +0100

    rename can now clear name, remove echo's

--- rename-workspace.sh
+++ rename-workspace.sh
@@ -4,12 +4,13 @@
 # keep the number after from before after the rename
 
 currentname=$(i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].name')
-echo "$currentname"
 number="$(grep -Po '^[0-9]' <<< $currentname)"
-echo "$number"
-newname="$(echo "" | dmenu -p "(rename) $currentname")"
-echo "$newname"
-newname="$number: $newname"
-echo "$newname"
+newname="$(echo "" | dmenu -p "rename '$currentname':")"
+
+if [ -n "$newname" ]; then
+	newname="$number: $newname"
+else
+	newname="$number"
+fi
 
 i3-msg rename workspace "\"$currentname\"" to "\"$newname\""
