22974441de11e226e8df4ab6a3b3072c5b87baa0
Hugo <hugo.hornquist@gmail.com>
2016-02-22 21:14:54 +0100

    changed next and prev workspace to a unified step workspace

--- step-workspace.sh
+++ step-workspace.sh
@@ -6,14 +6,31 @@
 
 # TODO it wraps weirdly, and creates new workspaces a bit weird to
 
+echo "$1"
+
+if ! [ "$1" == "1" ] || [ "$1" == "-1" ]; then
+	echo "use step-workspace 1 to move to the next workspace"
+	echo "and -1 for previus workspace"
+	exit 1
+fi
+
+
 # get current display and workspace
 currentOut="$(i3-msg -t get_workspaces | jq -r 'map(select(.focused))[0].output')"
-workspaceNum=$(i3-msg -t get_workspaces | jq "map(select(.focused))[0].name" | sed -n  "s/^\"$currentOut:\([1-9]\|10\).*/\1/p")
+workspaceNum=$(
+		i3-msg -t get_workspaces |\
+		jq "map(select(.focused))[0].name" |\
+		sed -n  "s/^\"$currentOut:\([1-9]\|10\).*/\1/p")
+
 # you dont want to swith to the current workspace
-workspaceNum="$(($workspaceNum + 1))"
+workspaceNum="$(($workspaceNum + $1))"
 
 # Try moving to the next workspace over
-for x in $(seq "$workspaceNum" 10); do
+goalNum="10"
+if [ "$1" == "-1" ]; then
+	goalNum="1"
+fi
+for x in $(seq "$workspaceNum" "$1" "$goalNum"); do
 	# check if the workspace exists
 	wanted="$(i3-msg -t get_workspaces |\
 	   	jq "map(select(.output==\"$currentOut\"))[].name" |\
